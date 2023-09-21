35ef6af0fe924d5f86074c5138ab263f32c56aac
JordySoftware <jordarto@gmail.com>
2023-09-21 13:30:25 -0500

    Creacion de los modulos necesario (Facturas, Material)

---
+++ prueba-angular/src/app/facturas/facturas.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FacturasComponent } from './facturas/facturas.component';
+
+
+
+@NgModule({
+  declarations: [
+    FacturasComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FacturasModule { }
